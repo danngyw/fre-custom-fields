c14bf1aa82d513285bbabd0848d3f10b5a08fcb6
Dan Ngyen <wpcodev@gmail.com>
2021-01-18 19:04:28 +0700

    update

--- fre-custom-fields.php
+++ fre-custom-fields.php
@@ -18,8 +18,9 @@ function includes_fre_fields() {
 	define( 'FRE_FIELDS','fre_fields');
 	define( 'FRE_CUSTOM_FIELDS', dirname( __FILE__ ) . '/' );
 	define( 'FRE_CUSTOM_FIELDS_URL', plugins_url( '/' , __FILE__ ) );
-	include_once FRE_CUSTOM_FIELDS . '/class-fre-fields.php';
 	include_once FRE_CUSTOM_FIELDS . '/functions.php';
+	include_once FRE_CUSTOM_FIELDS . '/class-fre-fields.php';
+
 
 }
 add_action('after_setup_theme','includes_fre_fields', 99);
