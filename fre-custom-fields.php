1a5a82e18c2c545be8bdc43f3ee89c12368b3a7c
Dan Ngyen <wpcodev@gmail.com>
2021-01-18 19:07:18 +0700

    update

--- fre-custom-fields.php
+++ fre-custom-fields.php
@@ -12,26 +12,27 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly.
 }
 
-function fre_define_custom_fields(){
-	$fields = array();
-	// Define field 1
-	$fields[] = array(
-		'name' => 'custom_field_1',
-		'label' => "Custom Field 1",
-		'placeholder' => 'Field 1 placehoder',
-		'type' => 'text', // text/area/select/checkbox/radio
-		'required' => true,
-	);
-	// define fields 3
-	$fields[] = array(
-		'name' => 'custom_field_2',
-		'label' => "Custom Field 2",
-		'type' => 'textarea',
-		'placeholder' => 'Field 2 placehoder',
-	);
-	return $fields;
+if ( ! function_exists('fre_define_custom_fields') ){
+	function fre_define_custom_fields(){
+		$fields = array();
+		// Define field 1
+		$fields[] = array(
+			'name' => 'custom_field_1',
+			'label' => "Custom Field 1",
+			'placeholder' => 'Field 1 placehoder',
+			'type' => 'text', // text/area/select/checkbox/radio
+			'required' => true,
+		);
+		// define fields 3
+		$fields[] = array(
+			'name' => 'custom_field_2',
+			'label' => "Custom Field 2",
+			'type' => 'textarea',
+			'placeholder' => 'Field 2 placehoder',
+		);
+		return $fields;
+	}
 }
-
 function includes_fre_fields() {
 
 	// bootstrap the core plugin
